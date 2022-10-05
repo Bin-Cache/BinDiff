1dfbe40e7e06aef48a0816d3aea38f18659f3f3f
Minhaj-Rahat <rahat.minhajul@gmail.com>
2022-10-05 01:14:47 -0600

    removed hardcoded base address

--- ghidra_mods/ghidra_pipeline.sh
+++ ghidra_mods/ghidra_pipeline.sh
@@ -14,7 +14,7 @@ script="${1}/ghidra_mods/reference_trace.py"
 update_files="${1}/versions/*.bin"
 
 
-$GHIDRA_HEADLESS $proj_loc Evolution -import $update_files -loader BinaryLoader -loader-baseAddr 0x20004000 -loader-blockName app -processor ARM:LE:32:Cortex
+$GHIDRA_HEADLESS $proj_loc Evolution -import $update_files -loader BinaryLoader -loader-baseAddr $2 -loader-blockName app -processor ARM:LE:32:Cortex
 $GHIDRA_HEADLESS $proj_loc Evolution -process "*.bin" -scriptPath $script_fold -postScript $script $1 -noanalysis -readOnly
 
 
